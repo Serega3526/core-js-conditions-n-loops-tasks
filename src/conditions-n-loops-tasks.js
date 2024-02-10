62ef6422f1a3875bf99379ee6102b2aacdaa906c
Sergey Kruglov <serega-kolpino@inbox.ru>
2024-02-10 23:41:00 +0300

    feat: compleat 12/7

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -356,8 +356,23 @@ function isContainNumber(num, digit) {
  *  [2, 3, 9, 5] => 2       => 2 + 3 === 5 then balance element is 9 and its index = 2
  *  [1, 2, 3, 4, 5] => -1   => no balance element
  */
-function getBalanceIndex(/* arr */) {
-  throw new Error('Not implemented');
+function getBalanceIndex(arr) {
+  for (let i = 0; i < arr.length; i += 1) {
+    let summ1 = 0;
+    let summ2 = 0;
+    for (let a = 0; a < i; a += 1) {
+      summ1 += arr[a];
+    }
+
+    for (let j = i + 1; j < arr.length; j += 1) {
+      summ2 += arr[j];
+    }
+    if (summ1 === summ2) {
+      return i;
+    }
+  }
+
+  return -1;
 }
 
 /**
